68bc7e2aa0a40828a40222aac1cc80d525a5d477
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-06 01:43:18 +0300

    feat: Added <router-outlet> to layouts and imported SharedModule in all components.

--- frontend/src/app/components/layouts/layouts.component.ts
+++ frontend/src/app/components/layouts/layouts.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../common/shared/shared.module';
 
 @Component({
   selector: 'app-layouts',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './layouts.component.html',
   styleUrl: './layouts.component.css'
 })
