c83912471d5c44faecf0176778e342764eb65556
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-07 00:43:01 +0300

    feat: Created ValidDirective for input validation styling.

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../common/shared/shared.module';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  login(form: NgForm) {}
 }
