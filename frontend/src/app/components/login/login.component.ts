eed31feaad8e693f46c307738cc9c38a26d37d85
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-09 02:43:29 +0300

    feat: Implemented login function with authentication and toastr notification.

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -3,6 +3,9 @@ import { SharedModule } from '../../common/shared/shared.module';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from './services/auth.service';
+import { LoginModel } from './models/login.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +15,24 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private _toastr: ToastrService, private _spinner: NgxSpinnerService) {
-  }
+  constructor(
+    private _auth: AuthService,
+    private _toastr: ToastrService,
+    private _router: Router
+  ) {}
+
   login(form: NgForm) {
+    if (form.valid) {
+      let model = new LoginModel();
+      model.email = form.controls['email'].value;
+      model.password = form.controls['password'].value;
+
+      this._auth.login(model, (res) => {
+        this._toastr.success('Giriş başarılı!');
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('user', JSON.stringify(res.user));
+        this._router.navigateByUrl('/');
+      });
+    }
   }
 }
