03f2aa5a3b3f9ae4cc83eb3c33be8a210a35484a
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-11 00:49:42 +0300

    feat: Implemented register service in AuthService.

--- frontend/src/app/components/auth/components/services/auth.service.ts
+++ frontend/src/app/components/auth/components/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { GenericHttpService } from '../../../../../services/generic-http.service';
 import { LoginModel } from '../models/login.model';
-import { LoginResponseModel } from '../components/login/models/login-response.model';
+import { LoginResponseModel } from '../models/login-response.model';
+import { GenericHttpService } from '../../../../services/generic-http.service';
+import { RegisterModel } from '../models/register.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ export class AuthService {
 
   login(model:LoginModel, callBack: (res:LoginResponseModel)=> void){
     this._http.post<LoginResponseModel>("auth/login",model,res=>  callBack(res));
+  }
+
+
+  register(model:RegisterModel, callBack: (res:LoginResponseModel)=> void){
+    this._http.post<LoginResponseModel>("auth/register",model, res=> callBack(res));
   }
 }
