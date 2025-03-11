a6d49194e70740c5c20f44130a1054ef179b0aa7
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-12 01:29:34 +0300

    feat: Registered category routes in the application.

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,10 @@ app.use(express.json());
 app.use(cors());
 
 const authRouter = require("./routers/auth.router");
-
+const categoryRouter = require("./routers/category.router");
 
 app.use("/api/auth", authRouter);
+app.use("/api/categories",categoryRouter);
 
 connection();
 
