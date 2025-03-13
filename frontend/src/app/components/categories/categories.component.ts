71b4c4847b1b67df1b9d5ccacae6336822da5ef8
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-14 01:27:48 +0300

    feat: Implemented getAll method in categories component.

--- frontend/src/app/components/categories/categories.component.ts
+++ frontend/src/app/components/categories/categories.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../common/shared/shared.module';
+import { CategoryModel } from './models/category.model';
+import { ToastrService } from 'ngx-toastr';
+import { CategoryService } from './services/category.service';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
+  categories: CategoryModel[] = [];
 
+  constructor(
+    private _toastr: ToastrService,
+    private _category: CategoryService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this._category.getAll((res) => (this.categories = res));
+  }
 }
