d797376e52500c3950adba82fdaa5d1d116e431d
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-21 00:45:04 +0300

    feat: Added add method in ProductService for adding products.

--- frontend/src/app/components/products/services/product.service.ts
+++ frontend/src/app/components/products/services/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { GenericHttpService } from '../../../services/generic-http.service';
+import { MessageResponseModel } from '../../../common/models/message.response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private _http: GenericHttpService) { }
+
+  add(model: FormData, callBack: (res: MessageResponseModel)=> void){
+    this._http.post<MessageResponseModel>("products/add", model, res=> callBack(res));
+  }
+
 }
