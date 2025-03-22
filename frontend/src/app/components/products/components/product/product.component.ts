c80e11fdfcbd9dc7f1efec7cb47448b70f2a0fb8
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-23 01:37:03 +0300

    feat: Completed static HTML for Product component.

--- frontend/src/app/components/products/components/product/product.component.ts
+++ frontend/src/app/components/products/components/product/product.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../../common/shared/shared.module';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
