422374548d737f2421f1f6b4331d964dc49dd8c9
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-25 01:55:36 +0300

    feat: Implemented add function in ProductAddComponent with form validation and file upload.

--- frontend/src/app/components/products/components/product-add/product-add.component.ts
+++ frontend/src/app/components/products/components/product-add/product-add.component.ts
@@ -5,29 +5,61 @@ import { CategoryService } from '../../../categories/services/category.service';
 import { ToastrService } from 'ngx-toastr';
 import { ProductService } from '../../services/product.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-product-add',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './product-add.component.html',
-  styleUrl: './product-add.component.css'
+  styleUrl: './product-add.component.css',
 })
-export class ProductAddComponent  implements OnInit {
+export class ProductAddComponent implements OnInit {
   categories: CategoryModel[] = [];
   images: File[] = [];
   imageUrls: any[] = [];
 
-  constructor(  private _category: CategoryService,
+  constructor(
+    private _category: CategoryService,
     private _toastr: ToastrService,
     private _product: ProductService,
-    private _router: Router) {
-  }
+    private _router: Router
+  ) {}
   ngOnInit(): void {
     this.getCategories();
   }
 
   getCategories() {
-    this._category.getAll(res => this.categories = res);
+    this._category.getAll((res) => (this.categories = res));
+  }
+
+  add(form: NgForm) {
+    if (form.value['categoriesSelect'].length == 0) {
+      this._toastr.error('Kategori seçimi yapmadınız!');
+      return;
+    }
+    if (form.valid) {
+      let product = form.value;
+      let categories: string[] = product['categoriesSelect'];
+      let name = product['name'];
+      let price = product['price'];
+      let stock = product['stock'];
+      price = price.toString().replace(',', '.');
+      let formData = new FormData();
+      formData.append('name', name);
+      formData.append('stock', stock);
+      formData.append('price', price);
+      for (const category of categories) {
+        formData.append('categories', category);
+      }
+      for (const image of this.images) {
+        formData.append('images', image, image.name);
+      }
+      this._product.add(formData, (res) => {
+        this._toastr.success(res.message);
+        form.reset();
+        this.imageUrls = [];
+      });
+    }
   }
 }
