ef0b55a16f820023bc4898286939034835f41671
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-27 01:08:38 +0300

    feat: Completed static HTML for ProductUpdateComponent.

--- frontend/src/app/components/products/components/product-update/product-update.component.ts
+++ frontend/src/app/components/products/components/product-update/product-update.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../../common/shared/shared.module';
 
 @Component({
   selector: 'app-product-update',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './product-update.component.html',
   styleUrl: './product-update.component.css'
 })
