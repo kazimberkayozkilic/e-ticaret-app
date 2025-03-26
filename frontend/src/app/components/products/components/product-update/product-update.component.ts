73a7e37c5b8fd0548a0d92edca1b56cd1ee791e0
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-27 01:17:22 +0300

    feat: Added getCategories method to fetch all categories in ProductUpdateComponent.

--- frontend/src/app/components/products/components/product-update/product-update.component.ts
+++ frontend/src/app/components/products/components/product-update/product-update.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../../common/shared/shared.module';
+import { CategoryModel } from '../../../categories/models/category.model';
+import { ProductModel } from '../../models/product.model';
+import { CategoryService } from '../../../categories/services/category.service';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-update',
@@ -9,5 +15,25 @@ import { SharedModule } from '../../../../common/shared/shared.module';
   styleUrl: './product-update.component.css'
 })
 export class ProductUpdateComponent {
+  categories: CategoryModel[] = [];
+  images: File[] = [];
+  imageUrls: any[] = [];
+  productId: string = "";
+  product: ProductModel = new ProductModel();
 
+  constructor(
+    private _category: CategoryService,
+    private _toastr: ToastrService,
+    private _product: ProductService,
+    private _router: Router,
+    private _activated: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this._category.getAll(res => this.categories = res);
+  }
 }
