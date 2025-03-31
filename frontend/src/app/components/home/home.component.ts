ea31436c3a92f66fdf689da0e16209d625f682d6
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-03-31 03:32:28 +0300

    feat: Added getAll function to fetch products for the homepage.

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../common/shared/shared.module';
+import { ProductModel } from '../products/models/product.model';
+import { RequestModel } from '../../common/models/request.model';
+import { CategoryModel } from '../categories/models/category.model';
+import { CategoryService } from '../categories/services/category.service';
+import { ProductService } from '../products/services/product.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +14,22 @@ import { SharedModule } from '../../common/shared/shared.module';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  categories: CategoryModel[] = [];
+  request: RequestModel = new RequestModel();
+  products: ProductModel[] = [];
 
+  constructor(
+    private _category: CategoryService,
+    private _product: ProductService,
+    private _toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
+    this._product.getAllForHomePage(this.request, res=> this.products = res);
+  }
 }
