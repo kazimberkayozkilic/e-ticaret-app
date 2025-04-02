95de23b5313aab631411352bd6b0bba9f80e1c02
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-04-02 20:25:16 +0300

    feat: Added add method to BasketService with user assignment and count update.

--- frontend/src/app/components/baskets/services/basket.service.ts
+++ frontend/src/app/components/baskets/services/basket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GenericHttpService } from '../../../services/generic-http.service';
 import { BasketModel } from '../models/basket.model';
+import { MessageResponseModel } from '../../../common/models/message.response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,15 @@ export class BasketService {
     let user = JSON.parse(userString);
     let model = {userId: user._id};
     this._http.post<any>("baskets/getCount",model, res=> this.count = res.count);
+  }
+
+  add(model:BasketModel, callBack: (res: MessageResponseModel)=> void){
+    let userString = localStorage.getItem("user");
+    let user = JSON.parse(userString);
+    model.userId = user._id;
+    this._http.post<MessageResponseModel>("baskets/add",model, res=>{
+      this.getCount();
+      callBack(res);
+    });
   }
 }
