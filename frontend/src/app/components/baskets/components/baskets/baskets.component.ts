a9acaddc892209691ede20b441afca63cb059945
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-04-08 05:37:38 +0300

    feat: Added getAll method to fetch and assign baskets in BasketsComponent.

--- frontend/src/app/components/baskets/components/baskets/baskets.component.ts
+++ frontend/src/app/components/baskets/components/baskets/baskets.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../../common/shared/shared.module';
+import { BasketService } from '../../services/basket.service';
+import { ToastrService } from 'ngx-toastr';
+import { SwalService } from '../../../../services/swal.service';
+import { BasketModel } from '../../models/basket.model';
 
 @Component({
   selector: 'app-baskets',
@@ -9,5 +13,23 @@ import { SharedModule } from '../../../../common/shared/shared.module';
   styleUrl: './baskets.component.css'
 })
 export class BasketsComponent {
+  baskets: BasketModel[] = [];
+  sum: number = 0;
+
+  constructor(
+    private _basket: BasketService,
+    private _toastr: ToastrService,
+    private _swal: SwalService
+  ){}
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+getAll(){
+  this._basket.getAll(res=> {
+    this.baskets = res;
+  });
+}
 
 }
