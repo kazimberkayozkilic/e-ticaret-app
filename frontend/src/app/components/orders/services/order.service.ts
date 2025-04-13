2282730809fe9e3b782ecbc22f590982fb1291d1
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-04-13 16:06:21 +0300

    feat: Added create function to OrderService for placing new orders.

--- frontend/src/app/components/orders/services/order.service.ts
+++ frontend/src/app/components/orders/services/order.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { GenericHttpService } from '../../../services/generic-http.service';
+import { BasketService } from '../../baskets/services/basket.service';
+import { MessageResponseModel } from '../../../common/models/message.response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  constructor(  private _http: GenericHttpService,
+    private _basket: BasketService) { }
+
+    create(callBack: (res: MessageResponseModel)=> void){
+      let userString = localStorage.getItem("user");
+      let user = JSON.parse(userString);
+      let model = {userId: user._id};
+      this._http.post<MessageResponseModel>("orders/create",model,res=> {
+        this._basket.getCount();
+        callBack(res);
+      });
+    }
 }
